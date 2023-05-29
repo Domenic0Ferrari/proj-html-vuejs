f1af42fc10d725b4997a7388886784040ab081f7
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-05-29 19:31:53 +0200

    add icon in header-top

--- src/components/appHeader.vue
+++ src/components/appHeader.vue
@@ -1,5 +1,5 @@
 <script>
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 export default {
     data() {
         return {
@@ -7,16 +7,31 @@ export default {
         }
     },
     components: {
-        faFacebookF
+        faFacebookF,
+        faTwitter,
+        faInstagram,
+        faYoutube
     }
 }
 </script>
 
 <template>
     <header>
-        <div class="header_top mb-3">
+        <div class="header_top">
             <div class="container">
-                <font-awesome-icon icon="fa-brands fa-facebook-f" style="color: #494949;" />
+                <div class="row d-flex">
+                    <div class="col-3 d-flex align-items-center gap-3">
+                        <a href="#"><font-awesome-icon icon="fa-brands fa-facebook-f" style="color: #494949;" /></a>
+                        <a href="#"><font-awesome-icon icon="fa-brands fa-twitter" style="color: #494949;" /></a>
+                        <a href="#"><font-awesome-icon icon="fa-brands fa-instagram" style="color: #494949;" /></a>
+                        <a href="#"><font-awesome-icon icon="fa-brands fa-youtube" style="color: #494949;" /></a>
+                    </div>
+                    <div class="col-9 d-flex justify-content-end align-items-center">
+                        <button>Shopping Cart</button>
+                        <button>My Account</button>
+                        <button>Cart</button>
+                    </div>
+                </div>
             </div>
         </div>
         <div class="header_bottom">
@@ -34,6 +49,14 @@ header {
     .header_top {
         height: 80px;
         border-bottom: 1px solid $brand_second;
+
+        .container {
+            height: 100%;
+
+            .row {
+                height: 100%;
+            }
+        }
     }
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faFacebookF } from '@fortawesome/free-brands-svg-icons'
+import { faFacebookF, faTwitter, faInstagram, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faFacebookF)
+library.add(faFacebookF, faTwitter, faInstagram, faYoutube)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
