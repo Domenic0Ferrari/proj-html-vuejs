33c1eb133fb28b95f6fe39dbf831c50c44e1c64f
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-05-29 12:55:34 +0200

    add appHeader, appMain, appFooter and install Sass

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script>
+import appHeader from './components/appHeader.vue';
+import appMain from './components/appMain.vue';
+import appFooter from './components/appFooter.vue';
+export default {
+    data() {
+        return {
+
+        }
+    },
+    components: {
+        appHeader,
+        appMain,
+        appFooter,
+    }
+}
+</script>
+
+<template>
+    <appHeader />
+    <appMain />
+    <appFooter />
+</template>
+
+<style scoped></style>
