7572dece80978085829bd4ec6802ba28b8f2be72
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-05-30 14:39:49 +0200

    add cards in products Main

--- src/components/productsMain.vue
+++ src/components/productsMain.vue
@@ -5,19 +5,47 @@ export default {
 </script>
 
 <template>
-    <div class="container d-flex flex-column align-items-center gap-3 mt-5">
-        <h3>Featured Products</h3>
+    <div class="container d-flex flex-column align-items-center gap-3 mt-5 mb-5">
+        <div class="my-head d-flex align-itmes-center justify-content-between">
+            <hr>
+            <h3>Featured Products</h3>
+            <hr>
+        </div>
         <h5>Must have products from our top sellers</h5>
         <div>
             <button>MEN</button>
             <button>WOMAN</button>
             <button>ACCESSORIES</button>
         </div>
-        <div class="contain-card d-flex justify-content-between">
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
-            <div class="card"></div>
+        <div class="contain-card d-flex justify-content-between gap-3">
+            <div class="card d-flex flex-column gap-2">
+                <img src="../assets/images/black_elegant_leather_jacket-200x260.jpg" alt="#">
+                <h6>Black Leather Jacket</h6>
+                <div class="my-style">Men, Jackets, Jeans</div>
+                <div class="d-flex gap-3 align-items-end">
+                    <span class="my-price">$235</span>
+                    <span>$200</span>
+                </div>
+
+            </div>
+            <div class="card d-flex flex-column gap-2">
+                <img src="../assets/images/black_leather_suit-200x260.jpg" alt="#">
+                <h6>Black Leather Jacket</h6>
+                <div class="my-style">Men, Jackets</div>
+                <span>$176</span>
+            </div>
+            <div class="card d-flex flex-column gap-2">
+                <img src="../assets/images/blue_jacket_and_white_stripe_tee-200x260.jpg" alt="#">
+                <h6>Black Leather Jacket</h6>
+                <div class="my-style">Men, Jackets, Suits</div>
+                <span>$580</span>
+            </div>
+            <div class="card d-flex flex-column gap-2">
+                <img src="../assets/images/modern_black_leather_suit-200x260.jpg" alt="#">
+                <h6>Black Leather Jacket</h6>
+                <div class="my-style">Men, Jackets</div>
+                <span>$96</span>
+            </div>
         </div>
     </div>
 </template>
@@ -25,9 +53,19 @@ export default {
 <style lang="scss" scoped>
 @use '../assets/style/general.scss' as *;
 
-h3 {
-    font-size: 35px;
+.my-head {
+    width: 100%;
+
+    hr {
+        width: 450px;
+    }
+
+    h3 {
+        font-size: 35px;
+    }
 }
+
+
 
 h5 {
     font-size: 14px;
@@ -42,11 +80,24 @@ button {
 
 .contain-card {
     width: 100%;
-}
 
-.card {
-    height: 300px;
-    width: 250px;
-    border: 1px solid black;
+    .card {
+        width: calc(100% / 4);
+        border: 0;
+
+        .my-style {
+            font-size: 13px;
+            color: $brand_second;
+        }
+
+        span {
+            color: $brand_third;
+        }
+
+        .my-price {
+            font-size: 12px;
+            text-decoration: line-through;
+        }
+    }
 }
 </style>
