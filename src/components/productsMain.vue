c3ca883c3a26b7156f633ef5f9ebe4d32eebfdbd
Domenic0Ferrari <domenico99ferrari@gmail.com>
2023-05-30 14:44:36 +0200

    add appCard.vue

--- src/components/productsMain.vue
+++ src/components/productsMain.vue
@@ -1,6 +1,10 @@
 <script>
-export default {
+import AppCard from './appCard.vue';
 
+export default {
+    components: {
+        AppCard
+    }
 }
 </script>
 
@@ -18,34 +22,7 @@ export default {
             <button>ACCESSORIES</button>
         </div>
         <div class="contain-card d-flex justify-content-between gap-3">
-            <div class="card d-flex flex-column gap-2">
-                <img src="../assets/images/black_elegant_leather_jacket-200x260.jpg" alt="#">
-                <h6>Black Leather Jacket</h6>
-                <div class="my-style">Men, Jackets, Jeans</div>
-                <div class="d-flex gap-3 align-items-end">
-                    <span class="my-price">$235</span>
-                    <span>$200</span>
-                </div>
-
-            </div>
-            <div class="card d-flex flex-column gap-2">
-                <img src="../assets/images/black_leather_suit-200x260.jpg" alt="#">
-                <h6>Black Leather Jacket</h6>
-                <div class="my-style">Men, Jackets</div>
-                <span>$176</span>
-            </div>
-            <div class="card d-flex flex-column gap-2">
-                <img src="../assets/images/blue_jacket_and_white_stripe_tee-200x260.jpg" alt="#">
-                <h6>Black Leather Jacket</h6>
-                <div class="my-style">Men, Jackets, Suits</div>
-                <span>$580</span>
-            </div>
-            <div class="card d-flex flex-column gap-2">
-                <img src="../assets/images/modern_black_leather_suit-200x260.jpg" alt="#">
-                <h6>Black Leather Jacket</h6>
-                <div class="my-style">Men, Jackets</div>
-                <span>$96</span>
-            </div>
+            <AppCard />
         </div>
     </div>
 </template>
@@ -65,8 +42,6 @@ export default {
     }
 }
 
-
-
 h5 {
     font-size: 14px;
     color: $brand_first;
@@ -80,24 +55,5 @@ button {
 
 .contain-card {
     width: 100%;
-
-    .card {
-        width: calc(100% / 4);
-        border: 0;
-
-        .my-style {
-            font-size: 13px;
-            color: $brand_second;
-        }
-
-        span {
-            color: $brand_third;
-        }
-
-        .my-price {
-            font-size: 12px;
-            text-decoration: line-through;
-        }
-    }
 }
 </style>
